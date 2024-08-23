0cd7b8797e4f889b944197ca4b7a240cbf431528
Fernando MENDI <pfmendi@msp.amadeus.net>
2024-08-23 18:37:41 +0200

    added Status variables

--- wordlesolver/common/variables.py
+++ wordlesolver/common/variables.py
@@ -1,3 +1,16 @@
 class Language:
     ES: str = "es"
     EN: str = "en"
+
+class Status:
+    def __init__(self, code: str, color: str):
+        self.code: str = code
+        self.color: str = color
+
+    def __repr__(self):
+        return f"{self.code} : #{self.color}"
+
+class Answer:
+    CORRECT: Status = Status("0", "43a047")
+    MISPLACED: Status = Status("1", "e4a81d")
+    ABSENT: Status = Status("2", "757575")
