bfecde2d9549f30a959ea1deed064ccf82b92f66
Fernando MENDI <pfmendi@msp.amadeus.net>
2024-08-27 14:36:40 +0200

    use custom exceptions in validation

--- wordlesolver/common/validation.py
+++ wordlesolver/common/validation.py
@@ -1,11 +1,13 @@
 import re
 
+from wordlesolver.common.core import exceptions
+
 import pandas as pd
 
 
 def validate_word(word: str, language: str) -> bool:
     """
-    Validates whether a given word exists in the word list for a specified language. Raises an exception if the word does not exist.
+    Validates whether a given word exists in the word list for a specified language. Additionally checks if the word is exactly 5 letters long. Raises exceptions if the word is invalid.
 
     Parameters
     ----------
@@ -17,22 +19,24 @@ def validate_word(word: str, language: str) -> bool:
     Returns
     -------
     bool
-        True if the word exists in the word list.
+        True if the word is valid and exists in the word list.
 
     Raises
     ------
-    ValueError
+    InvalidWordLengthError
+        If the word is not exactly 5 letters long.
+    WordNotFoundError
         If the word does not exist in the word list.
     """
 
     if len(word) != 5:
-        raise ValueError(f"The word '{word}' is not 5 characters long.")
+        raise exceptions.InvalidWordLengthError(word)
 
     words: pd.DataFrame = pd.read_csv(f"data/{language}/words.csv")
     is_word: bool = any(words.word == word)
 
     if not is_word:
-        raise ValueError(f"The word '{word}' does not exist in the {language.upper()} word list.")
+        raise exceptions.WordNotFoundError(word, language)
 
     return is_word
 
@@ -53,14 +57,15 @@ def validate_answer(answer: str) -> bool:
 
     Raises
     ------
-    ValueError
+    InvalidAnswerError
         If the answer string is not valid.
     """
 
+    # Compile a regular expression pattern that matches exactly 5 characters, each '0', '1', or '2'.
     pattern: re.Pattern = re.compile("^[012]{5}$")
     is_answer: bool = pattern.match(answer)
 
     if not is_answer:
-        raise ValueError(f"The answer '{answer}' is not a valid Wordle-style answer. It must be exactly 5 characters long, with each character being '0', '1', or '2'.")
+        raise exceptions.InvalidAnswerError(answer)
 
     return is_answer
