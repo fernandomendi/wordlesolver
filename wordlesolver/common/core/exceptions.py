0272662018c0e50c8e0b0cab2b240ab307bbc724
Fernando MENDI <pfmendi@msp.amadeus.net>
2024-08-29 00:58:00 +0200

    added exceptions and validations for guess weight

--- wordlesolver/common/core/exceptions.py
+++ wordlesolver/common/core/exceptions.py
@@ -25,3 +25,11 @@ class InvalidAnswerError(Exception):
         self.answer = answer
         self.message = f"The answer '{answer}' is not a valid Wordle-style answer. It must be exactly 5 characters long, with each character being '0', '1', or '2'."
         super().__init__(self.message)
+
+
+class InvalidWeightError(Exception):
+    """Exception raised when a weight is not a value between 0 and 1."""
+    def __init__(self, weight: float):
+        self.weight = weight
+        self.message = f"The value '{weight}' is not a valid guess weight. It must be a value between 0 and 1."
+        super().__init__(self.message)
